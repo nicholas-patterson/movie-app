cc3f43d0de1a7e1e507b46e6ad39bcc53a84c94f
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-08 03:25:54 -0400

    :sparkles: adds react portal to create modal
    adds react portal to create modal

---
+++ src/components/Modal.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const portal = document.getElementById("portal-root");
+
+class Modal extends Component {
+  constructor() {
+    super();
+    this.el = document.createElement("div");
+  }
+
+  componentDidMount() {
+    portal.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    portal.removeChild(this.el);
+  }
+
+  render() {
+    const { children } = this.props;
+    return ReactDOM.createPortal(children, this.el);
+  }
+}
+
+export default Modal;
