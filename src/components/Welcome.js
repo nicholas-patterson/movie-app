d71c4a25568e54bdcaac02e75fe696ca1fe1b5cf
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-07 23:38:16 -0400

    :sparkles: adds welcome component boilerplate
    adds welcome component boilerplate

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Welcome = () => {
-  return <h4>Welcome Component Here</h4>;
+  return (
+    <div className="bg-gray-900 h-screen px-24 ">
+      <h3>Welcome Page Here</h3>
+    </div>
+  );
 };
 
 export default Welcome;
