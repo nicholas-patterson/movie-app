1af3d7169ac971b56089a24fe0748dd00ebd46df
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-08 17:38:32 -0400

    :sparkles: adds trending movie component for individual movie
    adds trending movie component for individual movie

---
+++ src/components/TrendingMovie.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TrendingMovie = ({ movies }) => {
+  console.log("EACH MOVIE", movies);
+  return (
+    <>
+      <h3>Movies Below</h3>
+      <p>Told ya so !</p>
+    </>
+  );
+};
+
+export default TrendingMovie;
