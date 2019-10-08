d27e08ee8546a0a3eed06b192d62de38a8f2436b
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-07 23:37:34 -0400

    :sparkles: adds nav with tailwind utilities
    adds nav with tailwind utilities

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../images/mdi_movie_filter.png";
+import login from "../images/mdi-people.png";
+
+const Header = () => {
+  return (
+    <nav className="bg-gray-900 flex py-5 justify-between items-center px-24">
+      <div className="flex justify-between">
+        <img className="pr-3 w-12" src={logo} alt="take one" />
+        <h3 className="text-xl text-white font-light">
+          Movie.<span className="font-thin italic">me</span>
+        </h3>
+      </div>
+      <div>
+        <img className="w-10" src={login} alt="login" />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
