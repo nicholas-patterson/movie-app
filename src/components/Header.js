5aada78c2341856c917cf1c99e9a2897fe71f9f2
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-08 13:04:26 -0400

    :sparkles: adds svg icon
    adds svg icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import GroupSharpIcon from "@material-ui/icons/GroupSharp";
 import MovieFilterIcon from "@material-ui/icons/MovieFilter";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "./Modal";
+import Button from "@material-ui/core/Button";
+import "../styles/modal.css";
+import SvgIcon from '@material-ui/core/SvgIcon';
+
 
 const useStyles = makeStyles({
   root: {
@@ -10,7 +14,9 @@ const useStyles = makeStyles({
     "&:hover": {
       cursor: "pointer"
     }
-  }
+  },
+  google: {},
+  facebook: {}
 });
 
 const Header = () => {
@@ -40,8 +46,18 @@ const Header = () => {
       </nav>
       {isShowing ? (
         <Modal>
-          <div>
-            <h1>Login</h1>
+          <div className="main">
+            <h1 className="text-white text-center text-2xl font-light">
+              Login
+            </h1>
+            <Button
+              variant="contained"
+              startIcon={<SvgIcon><path fill="#00000" d="M23,11H21V9H19V11H17V13H19V15H21V13H23M8,11V13.4H12C11.8,14.4 10.8,16.4 8,16.4C5.6,16.4 3.7,14.4 3.7,12C3.7,9.6 5.6,7.6 8,7.6C9.4,7.6 10.3,8.2 10.8,8.7L12.7,6.9C11.5,5.7 9.9,5 8,5C4.1,5 1,8.1 1,12C1,15.9 4.1,19 8,19C12,19 14.7,16.2 14.7,12.2C14.7,11.7 14.7,11.4 14.6,11H8Z" /></SvgIcon>}
+              className={classes.google}
+            >
+              Login in with Google
+            </Button>
+            <Button>Login in with Facebook</Button>
           </div>
         </Modal>
       ) : null}
