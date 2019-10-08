acb96e46bafd8c083590c711fcf4d35a53659a1f
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-08 01:17:55 -0400

    :sparkles: adds border bottom flex and background to navbar
    adds border bottom flex and background to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import login from "../images/mdi-people.png";
 
 const Header = () => {
   return (
-    <nav className="bg-gray-900 flex py-5 justify-between items-center px-24">
+    <nav className="bg-gray-900 flex py-5 justify-between items-center px-24 border-teal-400 border-b-2">
       <div className="flex justify-between">
         <img className="pr-3 w-12" src={logo} alt="take one" />
         <h3 className="text-xl text-white font-light">
