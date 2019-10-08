d8ee8bc2617284260ff3c328cad44dadb7c1a133
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-08 16:07:23 -0400

    :sparkles: adds styling for all the buttons
    adds styling for all the buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import Modal from "./Modal";
 import Button from "@material-ui/core/Button";
 import "../styles/modal.css";
 import SvgIcon from "@material-ui/core/SvgIcon";
+import SimpleTabs from "./Tabs";
 
 const useStyles = makeStyles({
   root: {
@@ -60,23 +61,7 @@ const Header = () => {
       {isShowing ? (
         <Modal>
           <div className="main">
-            <h2 className="text-white text-center text-2xl font-light">
-              Login
-            </h2>
-            <div className="flex flex-col w-6/12 mx-auto">
-            <Button variant="contained" className={classes.google}>
-              <SvgIcon>
-                <path  fill="#fff" d="M21.35,11.1H12.18V13.83H18.69C18.36,17.64 15.19,19.27 12.19,19.27C8.36,19.27 5,16.25 5,12C5,7.9 8.2,4.73 12.2,4.73C15.29,4.73 17.1,6.7 17.1,6.7L19,4.72C19,4.72 16.56,2 12.1,2C6.42,2 2.03,6.8 2.03,12C2.03,17.05 6.16,22 12.25,22C17.6,22 21.5,18.33 21.5,12.91C21.5,11.76 21.35,11.1 21.35,11.1V11.1Z" />
-              </SvgIcon>
-              Login in with Google
-            </Button>
-            <Button variant="contained" className={classes.facebook}>
-              <SvgIcon>
-              <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z"/>
-              </SvgIcon>
-              Login in with Facebook
-              </Button>
-              </div>
+            <SimpleTabs/>
           </div>
         </Modal>
       ) : null}
