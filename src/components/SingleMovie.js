2da11b5de06230f395a43581f87325dd11a368e2
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-09 22:55:45 -0400

    :recycle: changed to index so I do not have to make an extra api call
    changed to index so I do not have to make an extra api call

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,26 +1,37 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { getMovie } from "../actions";
+import { getMovie, getRecommendations } from "../actions";
 import StarRatings from "react-star-ratings";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+}));
 
 const SingleMovie = props => {
-  console.log("PROPS", props);
+  const classes = useStyles();
+  let index = props.id;
+  // Convert the index that was typeof strinf to number
+  index = +index;
+  const myMovie = props.singleMovie[index];
+  console.log("MY MOVIE", myMovie);
   const baseURL = "https://image.tmdb.org/t/p/";
-  useEffect(() => {
-    props.getMovie(props.id);
-  }, []);
 
-  console.log(Math.floor(props.movie.vote_average / 2));
-
-  if (props.movie.length === 0) {
-    return <h3>Loading....</h3>;
+  if (!myMovie) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="bg-gray-600">
       <div
         style={{
-          backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),url(${baseURL}original${props.movie.backdrop_path})`,
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),url(${baseURL}original${myMovie.backdrop_path})`,
           height: "40vh",
           backgroundPosition: "top center",
           backgroundSize: "cover"
@@ -29,26 +40,33 @@ const SingleMovie = props => {
         <div className="flex items-center">
           <div className="pl-10 pt-12">
             <img
-              src={`${baseURL}w342${props.movie.poster_path}`}
-              alt={props.movie.title}
+              src={`${baseURL}w342${myMovie.poster_path}`}
+              alt={myMovie.title}
             />
           </div>
           <div className="leading-normal">
             <h2 className="text-white font-thin text-3xl pl-5">
-              {props.movie.title}
+              {myMovie.title}
             </h2>
             <h2 class="text-white font-thin w-6/12 pl-5 text-2xl">
-              {props.movie.overview}
+              {myMovie.overview}
             </h2>
             <StarRatings
-              className="ml-5"
-              rating={Math.floor(props.movie.vote_average / 2)}
+              className="stars"
+              rating={Math.floor(myMovie.vote_average / 2)}
               starRatedColor="Gold"
               starDimension="20px"
               numberOfStars={5}
             />
           </div>
         </div>
+      </div>
+      <div className={classes.root}>
+        {/* <List>
+          {props.recommended.map(rec => {
+            return <ListItem>{rec.title}</ListItem>;
+          })}
+        </List> */}
       </div>
     </div>
   );
@@ -57,13 +75,16 @@ const SingleMovie = props => {
 const mapStateToProps = state => {
   console.log("MSTP", state);
   return {
-    movie: state.singleMovieReducer.movie
+    movie: state.singleMovieReducer.movie,
+    recommended: state.recommendationReducer.recommendations,
+    singleMovie: state.trendingReducer.trendingMovies || null
   };
 };
 
 export default connect(
   mapStateToProps,
   {
-    getMovie
+    getMovie,
+    getRecommendations
   }
 )(SingleMovie);
