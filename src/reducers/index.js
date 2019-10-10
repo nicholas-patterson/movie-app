f0ca9e77e21a89b7efb4a835b5d9cf1daa2a6561
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-10 03:32:27 -0400

    :sparkles: adds rec reducer and video reducer to combine reducer
    adds rec reducer and video reducer to combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { trendingReducer } from "./trendingReducer";
 import { singleMovieReducer } from "./singleMovieReducer";
 import { recommendationReducer } from "./recommendationReducer";
+import { videoReducer } from "./videoReducer";
 
 export default combineReducers({
   trendingReducer,
   singleMovieReducer,
-  recommendationReducer
+  recommendationReducer,
+  videoReducer
 });
