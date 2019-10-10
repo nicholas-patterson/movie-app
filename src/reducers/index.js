01b4fc5a9950ba2ab19d1a9e706fa520bd8edce6
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-09 22:57:53 -0400

    :sparkles: adds recommendationReducer to combine reducers
    adds recommendationReducer to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { trendingReducer } from "./trendingReducer";
 import { singleMovieReducer } from "./singleMovieReducer";
+import { recommendationReducer } from "./recommendationReducer";
 
 export default combineReducers({
   trendingReducer,
-  singleMovieReducer
+  singleMovieReducer,
+  recommendationReducer
 });
