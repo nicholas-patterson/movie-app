6564e78a280d98583050184c7a5f95864e943989
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-10 03:14:38 -0400

    :sparkles: removed mui from comp
    removed mui from comp

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,21 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { getMovie, getRecommendations } from "../actions";
+import { getRecommendations } from "../actions";
 import StarRatings from "react-star-ratings";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
-  }
-}));
+import "../styles/main.css";
 
 const SingleMovie = props => {
-  const classes = useStyles();
+  console.log("PROPS INS SINGLE MOVIE", props);
+  useEffect(() => {
+    props.getRecommendations(props.singleMovie[index].id);
+  }, []);
+
   let index = props.id;
   // Convert the index that was typeof strinf to number
   index = +index;
@@ -23,9 +18,14 @@ const SingleMovie = props => {
   console.log("MY MOVIE", myMovie);
   const baseURL = "https://image.tmdb.org/t/p/";
 
-  if (!myMovie) {
-    return <div>Loading...</div>;
-  }
+  // Trim string to certain length and not stop in middle of a word.
+  let overview = myMovie.overview;
+  const maxLength = 220;
+  let trimmedString = overview.substr(0, maxLength);
+  trimmedString = trimmedString.substr(
+    0,
+    Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))
+  );
 
   return (
     <div className="bg-gray-600">
@@ -40,33 +40,52 @@ const SingleMovie = props => {
         <div className="flex items-center">
           <div className="pl-10 pt-12">
             <img
+              className="poster-width"
               src={`${baseURL}w342${myMovie.poster_path}`}
               alt={myMovie.title}
             />
           </div>
-          <div className="leading-normal">
+          <div className="leading-normal mt-8">
             <h2 className="text-white font-thin text-3xl pl-5">
-              {myMovie.title}
+              {myMovie.title}{" "}
+              <span className="text-lg pl-2">
+                ({myMovie.release_date.substr(0, 4)})
+              </span>
             </h2>
-            <h2 class="text-white font-thin w-6/12 pl-5 text-2xl">
-              {myMovie.overview}
+            <h2 className="text-white font-thin w-6/12 pl-5 text-2xl">
+              {trimmedString + "..."}
             </h2>
-            <StarRatings
-              className="stars"
-              rating={Math.floor(myMovie.vote_average / 2)}
-              starRatedColor="Gold"
-              starDimension="20px"
-              numberOfStars={5}
-            />
+            <div className="stars">
+              <StarRatings
+                rating={Math.floor(myMovie.vote_average / 2)}
+                starRatedColor="Gold"
+                starDimension="20px"
+                numberOfStars={5}
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className={classes.root}>
-        {/* <List>
-          {props.recommended.map(rec => {
-            return <ListItem>{rec.title}</ListItem>;
-          })}
-        </List> */}
+      <div className="bg-gray-900 flex">
+        <div className="w-2/4 border-r-2 border-400-teal">
+          <div className="overflow-scroll ht">
+            {props.recommended.map(rec => {
+              return (
+                <div>
+                  <img
+                    className="rec-list"
+                    alt={rec.title}
+                    src={`${baseURL}w342${rec.poster_path}`}
+                  />
+                  <h4>{rec.title}</h4>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="w-2/4 border-r-2 border-400-teal">
+          <h5>Misc. Here</h5>
+        </div>
       </div>
     </div>
   );
@@ -75,16 +94,14 @@ const SingleMovie = props => {
 const mapStateToProps = state => {
   console.log("MSTP", state);
   return {
-    movie: state.singleMovieReducer.movie,
     recommended: state.recommendationReducer.recommendations,
-    singleMovie: state.trendingReducer.trendingMovies || null
+    singleMovie: state.trendingReducer.trendingMovies
   };
 };
 
 export default connect(
   mapStateToProps,
   {
-    getMovie,
     getRecommendations
   }
 )(SingleMovie);
