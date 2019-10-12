790616f606d9c55874225c7c59db165fff884ea8
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-12 19:50:20 -0400

    :sparkles: adds search movie component
    adds search movie component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const SearchResult = ({ results, index }) => {
+  console.log("EACH MOVIE", results);
+  const baseURL = "https://image.tmdb.org/t/p/";
+  return (
+    <div className="mr-3 ml-3  mt-10">
+      <Link>
+        <img
+          className="poster-img"
+          src={`${baseURL}w342${results.poster_path}`}
+          alt={results.title}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default SearchResult;
