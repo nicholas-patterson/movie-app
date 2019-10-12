a1d2cc61900d91337a6506c2677fcc6963d67acd
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-12 12:56:47 -0400

    :sparkles: adds search movie reducer to combine reducers
    adds search movie reducer to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { trendingReducer } from "./trendingReducer";
 import { singleMovieReducer } from "./singleMovieReducer";
 import { recommendationReducer } from "./recommendationReducer";
 import { videoReducer } from "./videoReducer";
+import { searchMovieReducer } from "./searchMovieReducer";
 
 export default combineReducers({
   trendingReducer,
   singleMovieReducer,
   recommendationReducer,
-  videoReducer
+  videoReducer,
+  searchMovieReducer
 });
