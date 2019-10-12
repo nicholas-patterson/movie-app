506095b21ad513f598404c3eeb20fb518762d462
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-12 12:56:11 -0400

    :sparkles: adds search movie reducer
    adds search movie reducer

---
+++ src/reducers/searchMovieReducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+  searchedMovie: {
+    results: [],
+    isSearching: false
+  },
+  error: ""
+};
+
+export const searchMovieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_QUERY_START":
+      return {
+        ...state,
+        isSearching: true
+      };
+
+    case "FETCH_QUERY_SUCCESS":
+      return {
+        ...state,
+        searchedMovie: {
+          results: [...state.searchedMovie.results, action.payload],
+          page: action.payload.page,
+          total_pages: action.payload.total_pages,
+          total_results: action.payload.total_results,
+          isSearching: false
+        },
+        error: ""
+      };
+    case "FETCH_QUERY_FAILURE":
+      return {
+        ...state,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
