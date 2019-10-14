20dc199d7e4c42a68c7574ef90f79e0fb02f82b1
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-14 17:42:52 -0400

    :sparkles: adds reviews reducers
    adds reviews reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import { singleMovieReducer } from "./singleMovieReducer";
 import { recommendationReducer } from "./recommendationReducer";
 import { videoReducer } from "./videoReducer";
 import { searchMovieReducer } from "./searchMovieReducer";
+import { reviewsReducer } from "./reviewsReducer";
 
 export default combineReducers({
   trendingReducer,
   singleMovieReducer,
   recommendationReducer,
   videoReducer,
-  searchMovieReducer
+  searchMovieReducer,
+  reviewsReducer
 });
