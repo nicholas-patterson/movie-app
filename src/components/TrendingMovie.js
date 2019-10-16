4ee4451fb0e5735945cfeb52d2544a8899364f9a
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-16 16:22:52 -0400

    :art: adds fragment for upcoming additional code
    adds fragment for upcoming additional code

--- src/components/TrendingMovie.js
+++ src/components/TrendingMovie.js
@@ -5,15 +5,17 @@ import "../styles/main.css";
 const TrendingMovie = ({ movies, index }) => {
   const baseURL = "https://image.tmdb.org/t/p/";
   return (
-    <div data-tool-tip={movies.title} className="mr-3 ml-3  mt-10">
-      <Link to={`/movie/${index}`}>
-        <img
-          className="poster-img poster-div"
-          src={`${baseURL}w342${movies.poster_path}`}
-          alt={movies.title}
-        />
-      </Link>
-    </div>
+    <>
+      <div data-tool-tip={movies.title} className="mr-3 ml-3  mt-10">
+        <Link to={`/movie/${index}`}>
+          <img
+            className="poster-img poster-div"
+            src={`${baseURL}w342${movies.poster_path}`}
+            alt={movies.title}
+          />
+        </Link>
+      </div>
+    </>
   );
 };
 
