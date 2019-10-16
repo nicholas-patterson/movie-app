b962a948885383d794ac4d7be881a39e2a29000a
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-16 15:00:43 -0400

    :sparkles: adds css styles to component
    adds css styles to component

--- src/components/TrendingMovie.js
+++ src/components/TrendingMovie.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "@reach/router";
+import "../styles/main.css";
 
 const TrendingMovie = ({ movies, index }) => {
   const baseURL = "https://image.tmdb.org/t/p/";
   return (
-    <div className="mr-3 ml-3  mt-10">
+    <div data-tool-tip={movies.title} className="mr-3 ml-3  mt-10">
       <Link to={`/movie/${index}`}>
         <img
-          className="poster-img"
+          className="poster-img poster-div"
           src={`${baseURL}w342${movies.poster_path}`}
           alt={movies.title}
         />
