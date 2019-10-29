ccdb0ee1cc9f56d6bbdea3fc2277bfa7a7819ca3
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-29 19:47:42 -0400

    :sparkles: adds function for trending movie component to get filtered genres

--- src/components/TrendingMovie.js
+++ src/components/TrendingMovie.js
@@ -2,11 +2,27 @@ import React from "react";
 import { Link } from "@reach/router";
 import "../styles/main.css";
 
-const TrendingMovie = ({ movies, index }) => {
+const TrendingMovie = ({ movies, index, genres }) => {
+  const filteredGenres = () => {
+    const result = [];
+    movies.genre_ids.forEach(id => {
+      genres.forEach(genre => {
+        if (genre.id === id) {
+          result.push(genre.name);
+        }
+      });
+    });
+    return result;
+  };
+
   const baseURL = "https://image.tmdb.org/t/p/";
   return (
     <>
-      <div data-tool-tip={movies.title} className="mr-3 ml-3  mt-10">
+      <div
+        data-tool-tip={movies.title}
+        data-movie-genre={filteredGenres().join(", ")}
+        className="mr-3 ml-3  mt-10"
+      >
         <Link to={`/movie/${index}`}>
           <img
             className="poster-img poster-div"
