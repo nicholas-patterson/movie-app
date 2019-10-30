a09229dda38689e1d5271445cf3c6874a51a7129
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-10-30 18:31:08 -0400

    :sparkles: adds conditional for movie poster
    adds conditional for movie poster

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "@reach/router";
+import noImage from "../images/noImage.jpg";
 
 const SearchResult = ({ results, index }) => {
   const baseURL = "https://image.tmdb.org/t/p/";
+
   return (
     <div className="mr-3 ml-3  mt-10">
       <Link to={`/results/movie/${index}`}>
         <img
           className="poster-img"
-          src={`${baseURL}w342${results.poster_path}`}
+          src={
+            results.poster_path === null
+              ? noImage
+              : `${baseURL}w342${results.poster_path}`
+          }
           alt={results.title}
         />
       </Link>
