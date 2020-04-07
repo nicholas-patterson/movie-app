3c6218e206d35ea730453a5b2eb3fb6f49f210b1
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-07 19:28:13 -0400

    :wrench: added a tailwind config file for custom breakpoints
    added a tailwind config file for custom breakpoints

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+      xlg: { max: "1450px" },
+      lg: { max: "1024px" },
+      md: { max: "720px" },
+      sm: { max: "414px" },
+      xs: { max: "320px" },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
